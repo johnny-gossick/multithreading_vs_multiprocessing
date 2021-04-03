4b94ce861438e12486df8584647df1e670e39b40
Johnny <johnny@pop-os-desktop.fios-router.home>
2021-04-03 18:59:45 -0400

    added pandas results

--- mvm.py
+++ mvm.py
@@ -1,5 +1,4 @@
 from threading import Thread
-from random import sample
 from multiprocessing import Process, Value
 from datetime import datetime
 import numpy as np
@@ -34,6 +33,9 @@ def get_elapsed_time(start): return (datetime.now() - start).total_seconds()
 def get_random_numbers(number_count): return np.array(range(number_count))
 
 
+def get_speedup(original, other): return float(original) / float(other)
+
+
 def time_adding_serially(numbers_to_add):
     start = datetime.now()
     total = sum(numbers_to_add)
@@ -46,6 +48,14 @@ def time_adding_numpy(numbers_to_add):
     total = np.sum(numbers_to_add)
 
     return get_elapsed_time(start), total
+
+
+def time_adding_pandas(numbers_to_add):
+    df = pd.DataFrame({'numbers': numbers_to_add})
+    start = datetime.now()
+    total = df.sum()
+
+    return get_elapsed_time(start), total['numbers']
 
 
 def time_adding_concurrently(numbers_to_add, task_count, task_class_name):
@@ -70,14 +80,19 @@ def run_experiment(number_count, task_count):
     multithreading_time, multithreading_total = time_adding_concurrently(numbers_to_add, task_count, AddingThread)
     print('Adding w/ numpy...')
     numpy_time, numpy_total = time_adding_numpy(numbers_to_add)
+    print('Adding w/ pandas...')
+    pandas_time, pandas_total = time_adding_pandas(numbers_to_add)
+
+    time_list = [serial_time, multiprocess_time, multithreading_time, numpy_time, pandas_time]
 
     return pd.DataFrame({
-        'mode': ['serial', 'multiprocessing', 'multithreading', 'numpy'],
-        'time': [serial_time, multiprocess_time, multithreading_time, numpy_time],
-        'sum':  [serial_total, multiprocess_total, multithreading_total, numpy_total],
-        'data_size': [number_count, number_count, number_count, number_count],
-        'task_count': [1, task_count, task_count, None]
-    })
+        'mode': ['serial', 'multiprocessing', 'multithreading', 'numpy', 'pandas'],
+        'speedup': [get_speedup(serial_time, other_time) for other_time in time_list],
+        'time': time_list,
+        'parallel_factor': [1, task_count, task_count, None, None],
+        'sum':  [serial_total, multiprocess_total, multithreading_total, numpy_total, pandas_total],
+        'data_size': [number_count, number_count, number_count, number_count, number_count]
+    }).sort_values(by=['time', 'mode'], ascending=True)
 
 
 def main():
